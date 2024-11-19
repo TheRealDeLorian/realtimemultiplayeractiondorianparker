a67aa889a56a4a6f856188f0b387525331973f33
Dorian Cottle <thenewdorian21@gmail.com>
2024-11-19 10:05:37 -0700

    movement tests pass

--- client/Tests/VehicleMovementTests.test.ts
+++ client/Tests/VehicleMovementTests.test.ts
@@ -1,19 +1,17 @@
-import { expect, test } from 'vitest'
-import { moveVehicle } from '../src/logic/vehicleUtils'
-import { PlayerVehicle } from '../src/Data/PlayerVehicle'
+import { expect, test } from "vitest";
+import { moveVehicle } from "../src/logic/vehicleUtils";
+import { PlayerVehicle } from "../src/Data/PlayerVehicle";
 
-
-
-test('Creates a vehicle', () => { 
+test("Creates a vehicle", () => {
   const playerVehicle: PlayerVehicle = {
-  id: 1,
-  xpos: 100,
-  ypos: 100,
-  angleindegs: 0,
-  isLeft: false,
-  isRight: false,
-  isAccelerating: false
-  }
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 0,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: false,
+  };
 
   const expectedVehicle: PlayerVehicle = {
     id: 1,
@@ -22,9 +20,201 @@ test('Creates a vehicle', () => {
     angleindegs: 0,
     isLeft: false,
     isRight: false,
-    isAccelerating: false
-    }
+    isAccelerating: false,
+  };
 
-  
-  expect(moveVehicle(playerVehicle) ) 
-})
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("Turn left", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 0,
+    isLeft: true,
+    isRight: false,
+    isAccelerating: false,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: -45,
+    isLeft: true,
+    isRight: false,
+    isAccelerating: false,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("Turn right", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 0,
+    isLeft: false,
+    isRight: true,
+    isAccelerating: false,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 45,
+    isLeft: false,
+    isRight: true,
+    isAccelerating: false,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("No turn", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 0,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: false,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 0,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: false,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("accelerate on x axis", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 0,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 101,
+    ypos: 100,
+    angleindegs: 0,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("accelerate on y axis", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 90,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 101,
+    angleindegs: 90,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("accelerate on 45 degree angle", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 45,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 101,
+    ypos: 101,
+    angleindegs: 45,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("accelerate on 225 degree angle", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: 225,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 99,
+    ypos: 99,
+    angleindegs: 225,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
+test("accelerate on -45 degree angle", () => {
+  const playerVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 100,
+    ypos: 100,
+    angleindegs: -45,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  const expectedVehicle: PlayerVehicle = {
+    id: 1,
+    xpos: 101,
+    ypos: 99,
+    angleindegs: -45,
+    isLeft: false,
+    isRight: false,
+    isAccelerating: true,
+  };
+
+  expect(moveVehicle(playerVehicle)).toEqual(expectedVehicle);
+});
+
--- client/src/logic/vehicleUtils.ts
+++ client/src/logic/vehicleUtils.ts
@@ -1,5 +1,20 @@
 import { PlayerVehicle } from "../Data/PlayerVehicle";
 
-export function moveVehicle({ vehicle }: { vehicle: PlayerVehicle }) {
-  return vehicle;
+export function moveVehicle(vehicle: PlayerVehicle) {
+  const newVehicle = { ...vehicle };
+  if (newVehicle.isLeft) {
+    newVehicle.angleindegs -= 45;
+  }
+  if (newVehicle.isRight) {
+    newVehicle.angleindegs += 45;
+  }
+  if (newVehicle.isAccelerating) {
+    newVehicle.xpos += Math.round(
+      Math.cos((newVehicle.angleindegs * Math.PI) / 180)
+    );
+    newVehicle.ypos += Math.round(
+      Math.sin((newVehicle.angleindegs * Math.PI) / 180)
+    );
+  }
+  return newVehicle;
 }
